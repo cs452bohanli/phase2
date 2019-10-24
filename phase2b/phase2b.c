b641ce83b9c6e541692eb939a08b7f2a1353049f
bohan li <li.3.141592653589@gmail.com>
2019-10-23 22:10:50 -0700

    finished?;

--- phase2b/phase2b.c
+++ phase2b/phase2b.c
@@ -2,15 +2,32 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-
+#include <unistd.h>
 #include <usloss.h>
 #include <phase1.h>
-
+#include <time.h>
 #include "phase2Int.h"
 
 
 static int      ClockDriver(void *);
 static void     SleepStub(USLOSS_Sysargs *sysargs);
+static void 	checkIfIsKernel();
+// semaphores
+int mutex;
+
+void P(int sid) {
+	assert(P1_P(sid) == P1_SUCCESS);
+}
+
+void V(int sid) {
+	assert(P1_V(sid) == P1_SUCCESS);
+}
+
+typedef struct p {
+    int pid, startTime, duration, isAsleep, isActive;
+} Process; 
+
+Process processes[P1_MAXPROC];
 
 /*
  * P2ClockInit
@@ -20,16 +37,23 @@ static void     SleepStub(USLOSS_Sysargs *sysargs);
 void 
 P2ClockInit(void) 
 {
+	checkIfIsKernel();
     int rc;
 
     P2ProcInit();
-
+	rc = P1_SemCreate("mutex", 1, &mutex);
+	assert(rc == P1_SUCCESS);
     // initialize data structures here
+	for (int i = 0; i < P1_MAXPROC; i++) processes[i].isActive = FALSE;	
 
     rc = P2_SetSyscallHandler(SYS_SLEEP, SleepStub);
     assert(rc == P1_SUCCESS);
 
+	int pid;
     // fork the clock driver here
+	// P1EnableInterrupts();
+	rc = P1_Fork("clock driver", ClockDriver, NULL, USLOSS_MIN_STACK, 2, 0, &pid);
+	assert(rc == P1_SUCCESS);
 }
 
 /*
@@ -41,7 +65,10 @@ P2ClockInit(void)
 void 
 P2ClockShutdown(void) 
 {
-    // stop clock driver
+	checkIfIsKernel();
+	// stop clock driver 
+	int rc = P1_WakeupDevice(USLOSS_CLOCK_DEV, 0, 0, TRUE);
+	assert(rc == P1_SUCCESS);	
 }
 
 /*
@@ -63,8 +90,16 @@ ClockDriver(void *arg)
             break;
         }
         assert(rc == P1_SUCCESS);
-
+		
         // wakeup any sleeping processes whose wakeup time has arrived
+		P(mutex);
+		for (int i = 0; i < P1_MAXPROC; i++) {
+			if (processes[i].isActive) {
+				if (processes[i].duration * 1000000 <= now - processes[i].startTime)
+					processes[i].isAsleep = FALSE;
+			}
+		}
+		V(mutex);
     }
     return P1_SUCCESS;
 }
@@ -77,9 +112,37 @@ ClockDriver(void *arg)
 int 
 P2_Sleep(int seconds) 
 {
+	checkIfIsKernel();
+	if (seconds < 0) return P2_INVALID_SECONDS;
+	
     // add current process to data structure of sleepers
+	int i;
+	P(mutex);
+	for (i = 0; i < P1_MAXPROC; i++) {
+		if (!processes[i].isActive) break;
+	}
+	assert(i != P1_MAXPROC);
+	processes[i].isActive = TRUE;
+	processes[i].isAsleep = TRUE;
+	int now;
+	int rc = USLOSS_DeviceInput(USLOSS_CLOCK_DEV, 0, &now);
+	assert(rc == USLOSS_DEV_OK);
+	processes[i].startTime = now;
+	processes[i].duration = seconds;
+	V(mutex);
     // wait until sleep is complete
-    return P1_SUCCESS;
+	while(1) {
+		P(mutex);
+		if (!processes[i].isAsleep) {
+			V(mutex);
+			break;
+		}
+		V(mutex);
+	}
+	P(mutex);
+	processes[i].isActive = FALSE;
+    V(mutex);
+	return P1_SUCCESS;
 }
 
 /*
@@ -95,3 +158,13 @@ SleepStub(USLOSS_Sysargs *sysargs)
     sysargs->arg4 = (void *) rc;
 }
 
+/*
+ * Checks psr to make sure OS is in kernel mode, halting USLOSS if not. Mode bit
+ * is the LSB.
+ */
+void checkIfIsKernel(){ 
+    if ((USLOSS_PsrGet() & 1) != 1) {
+        USLOSS_Console("The OS must be in kernel mode!\n");
+        USLOSS_IllegalInstruction();
+    }
+}
